64d6936a2a84c1d2737ddd72e46dfaae0f37b283
sirledys <sirledys.rangel@gmail.com>
2023-05-28 12:58:14 -0500

    Primera Versión del Frontend

--- src/components/Manillas.jsx
+++ src/components/Manillas.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { db } from '../firebase'
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc  } from 'firebase/firestore'
 
 const Manillas = () => {
+
   return (
-    <div>manillas</div>
+    <div className='container mt-8'>
+        <h1 className='text-center'>CONSULTA EL PRECIO DE TU MANILLA</h1>
+        <hr></hr>
+
+        <div className='row'>
+           <div className='col-3'>
+              <form action="#">
+                 <h3 className='text-center'>Material</h3>
+                 <select name="Material" id="lang">
+                     <option value="Cuero">Cuero</option>
+                     <option value="Cuerda">Cuerda</option>
+                 </select>
+              </form>
+          </div>
+ 
+          <div className='col-3'>
+             <form action="#">
+                 <h3 className='text-center'>Dije</h3>
+                 <select name="Dije" id="lang">
+                     <option value="Martillo">Martillo</option>
+                     <option value="Ancla">Ancla</option>
+                 </select>
+             </form>
+         </div>
+
+          <div className='col-3'>
+              <form action="#">
+                 <h3 className='text-center'>Tipo</h3>
+                 <select name="Tipo" id="lang">
+                     <option value="Oro">Oro</option>
+                     <option value="Oro Rosado">Oro Rosado</option>
+                     <option value="Plata">Plata</option>
+                     <option value="Niquel">Niquel</option>
+                 </select>
+              </form>
+          </div>
+
+         <div className='col-3'>
+             <form action="#">
+                 <h3 className='text-center'>Divisa</h3>
+                 <select name="Divisa" id="lang">
+                     <option value="Pesos">Pesos</option>
+                     <option value="Dolares">Dolares</option>
+                 </select>
+               </form>
+         </div>
+     </div>
+     <input type="submit" value="Consultar" className='col-2'/>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Manillas from './components/manillas.jsx'
+import Manillas from './components/Manillas.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
